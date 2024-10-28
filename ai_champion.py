6be293c6c94e68980d03ac3377ccbd4a30b8d755
Tommy Loh <tlohyk@outlook.com>
2024-10-28 08:15:45 +0800

    new changes3

--- ai_champion.py
+++ ai_champion.py
@@ -3,6 +3,7 @@ import streamlit as st
 import pandas as pd
 import numpy as np
 from my_script import get_completion
+from my_script import get_completion_workpass
 from utility import check_password
 
 # region <--------- Streamlit App Configuration --------->
@@ -22,23 +23,44 @@ os.environ['OPENAPI_API_KEY'] = st.secrets['OPENAPI_API_KEY']
 
 def about_us():
   st.title("About Us Now")
-  st.write("This application aims to empower users with information regarding the Primary Care Plan (PCP) for foreign workers in Singapore. We leverage external data sources, user input, and advanced language processing to deliver accurate and helpful insights.")
+  st.write("This application aims to empower users with information regarding the Primary Care Plan (PCP) and Work Passes for foreign workers in Singapore. We leverage external data sources, user input, and advanced language processing to deliver accurate and helpful insights.")
   
   st.header("Objectives")
-  st.write("- Assist users with inquiries related to the PCP for foreign workers.")
-  st.write("- Provide clear and concise explanations of the PCP process.")
+  st.write("- Assist users with inquiries related to the PCP and Work Passes for foreign workers.")
+  st.write("- Provide clear and concise explanations of the PCP process and types of Work Pass.")
   st.write("- Utilize external data sources and user input to generate relevant information.")
-  st.write("- Simplify understanding of the PCP for employers.")
+  st.write("- Simplify understanding of informations for employers.")
 
   st.header("Feature")
-  st.write("An AI assistant to help with user's enquiry.")
+  st.write("An AI assistant to help with user's enquiry on information regarding to PCP  and Work Pass.")
 
   st.header("Data sources")
   st.write("URL: https://www.mom.gov.sg/primary-care-plan/what-is-pcp")
+  st.write("URL: https://www.mom.gov.sg/primary-care-plan/buying-a-pcp")
+  st.write("URL: https://www.mom.gov.sg/primary-care-plan/getting-primary-care-services")
+  st.write("URL: https://www.mom.gov.sg/passes-and-permits")
+  
+  st.header("Author")
+  st.write("Tommy Loh")
 
 def methodology():
   st.title("Methodology")
-  st.write("This is the Methodology page.")
+  st.header("Data flows")
+  st.write(f""" \n
+    1. The user interacts with the app interface (Streamlit), inputting a query into the text input area and submitting it to the backend for processing. \n
+    2. The nature of the topic (PCP or Work Pass) is determined, and the website information extractor is called with the correct set of URLs. \n
+    3. The context of the query is set based on the topic, including the query itself and the intended response format. \n
+    4. The prompt is crafted appropriately to prevent prompt injection. \n
+    5. The data is sent through the OpenAI API call, and the response is awaited. \n
+    6. The retrieved response is displayed back to the app interface. \n\n
+           """)
+  st.image("Flowchart-Prompt-Engineering.png")
+  st.header("Implementations")
+  st.write(f""" \n
+    - A my_script.py to have functions for extract information from websites, set context and create prompts, set environment configurations and to call OpenAI via API. \n
+    - A ai_champion.py to create app interface, format and ordering of the pages. \n
+    - Commit and push codes to GitHub, and have Streamlit create the app from GitHub files. \n
+           """)
 
 def primary_care_plan():
     st.title("MOM Primary Care Plan")
@@ -47,18 +69,23 @@ def primary_care_plan():
 
     # Output widget: Displaying the response
     if st.button("Submit"):
-        # You can use a language model like GPT-3 to generate a response based on the user query and the website content
-        # Here's a simplified example:
         assistant_res = get_completion(user_query)
-        response = f"You asked: {user_query}. \n\n{assistant_res}"  # Replace with actual response generation logic    
+        response = f"You asked: {user_query}. \n\n{assistant_res}"     
         st.write(response)
 
-def about_mom():
-  st.title("About MOM")
-  st.write("This is the About MOM page.")
+def work_passes():
+    st.title("Work Passes")
+    # Input widget: Text area for user query
+    user_wp_query = st.text_area("Ask your question about work pass:", height=100)
 
-page_names = ["About Us", "Methodology", "Primary Care Plan", "About MOM"]
-page_functions = [about_us, methodology, primary_care_plan, about_mom]
+    # Output widget: Displaying the response
+    if st.button("Submit"):
+        assistant_res = get_completion_workpass(user_wp_query)
+        response = f"You asked: {user_wp_query}. \n\n{assistant_res}"      
+        st.write(response)
+
+page_names = ["About Us", "Methodology", "Primary Care Plan", "Work Passes"]
+page_functions = [about_us, methodology, primary_care_plan, work_passes]
 
 selected_page = st.sidebar.selectbox("Select a Page", page_names)
 
--- my_script.py
+++ my_script.py
@@ -11,26 +11,53 @@ load_dotenv('.env')
 mykey= os.getenv("OPENAPI_API_KEY")
 client = OpenAI(api_key=mykey)
 
+def fetch_and_parse_urls(urls):
+    results = []
+    for url in urls:
+        try:
+            response = requests.get(url)
+            response.raise_for_status()  # Raise an exception for error HTTP statuses
+            soup = BeautifulSoup(response.content, 'html.parser')
+
+            # Extract the desired text from the HTML, adjust the selector as needed
+            text = soup.text.replace('\n', '')
+            results.append(text)
+
+        except requests.exceptions.RequestException as e:
+            print(f"Error fetching {url}: {e}")
+            results.append(f"Error fetching {url}")
+
+    return results
+
+pcp_urls = [
+    "https://www.mom.gov.sg/primary-care-plan/what-is-pcp",
+    "https://www.mom.gov.sg/primary-care-plan/buying-a-pcp",
+    "https://www.mom.gov.sg/primary-care-plan/getting-primary-care-services"
+]
+
+
 # This is a function that send input (i.e., prompt) to LLM and receive the output from the LLM
 def get_completion(user_input):
-    url = "https://www.mom.gov.sg/primary-care-plan/what-is-pcp"
-    response = requests.get(url)
-    soup = BeautifulSoup(response.content, 'html.parser')
-    final_text = soup.text.replace('\n', '')
+    final_text = fetch_and_parse_urls(pcp_urls)
     prompt = f"""
-        Data source website: https://www.mom.gov.sg/primary-care-plan/what-is-pcp.
-        This website is about Primary Care Plan (PCP) introduced by the Ministry of Manpower.
+        This data source of the website is about Primary Care Plan (PCP) introduced by the Ministry of Manpower.
         The text delimited by triple backticks are scraped from the website and parsed using `html.parser`.
         Please answer the user's enquiry about the PCP in a professional and informative manner. If applicable, include guiding steps in your response.
         ```
         {final_text}
         ```
         """
+    user_prompt = f"""
+        The text by quadruple backticks is the user inputs:
+        ````
+        {user_input}
+        ````
+    """
     model="gpt-4o-mini"
     response = client.chat.completions.create(
         model=model,
         messages = [{"role": "system", "content": prompt},
-                    {"role": "user", "content": user_input}],
+                    {"role": "user", "content": user_prompt}],
         temperature=0, # this is the degree of randomness of the model's output
         top_p=1.0,
         max_tokens=4096,
@@ -38,17 +65,37 @@ def get_completion(user_input):
     )
     return response.choices[0].message.content
 
+workpass_urls = [
+    "https://www.mom.gov.sg/passes-and-permits"
+]
 
+# This is a function that send input (i.e., prompt) to LLM and receive the output from the LLM
+def get_completion_workpass(user_input):
+    final_text = fetch_and_parse_urls(workpass_urls)
+    prompt = f"""
+        This data source of the website is about work passes introduced by the Ministry of Manpower.
+        The text delimited by triple backticks are scraped from the website and parsed using `html.parser`.
+        Please answer the user's enquiry about the work passes in a professional and informative manner. If applicable, include guiding steps in your response.
+        ```
+        {final_text}
+        ```
+        """
+    user_prompt = f"""
+        The text by quadruple backticks is the user inputs:
+        ````
+        {user_input}
+        ````
+    """
+    model="gpt-4o-mini"
+    response = client.chat.completions.create(
+        model=model,
+        messages = [{"role": "system", "content": prompt},
+                    {"role": "user", "content": user_prompt}],
+        temperature=0, # this is the degree of randomness of the model's output
+        top_p=1.0,
+        max_tokens=4096,
+        n=1
+    )
+    return response.choices[0].message.content
 
-
-
-
-""" user_input = "What is this PCP? Do I need it? And how can I apply it?"
-len(final_text.split())
-
-
-# This example shows the use of angled brackets <> as the delimiters
-
-response = get_completion(prompt, user_input)
-print(response) """
 
